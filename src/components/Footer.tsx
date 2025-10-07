1bd0145c3fc9addf188d61a2a115bb079775554b
viyom shukla <viyomshukla21@gmail.com>
2025-10-07 17:18:18 +0530

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-secondary mt-auto">
       <div className="container mx-auto px-4 py-6">
         <div className="text-center text-muted-foreground">
-          © Made with ❤️ by Prateek And Piyush
+          © Made with ❤️ 
         </div>
       </div>
     </footer>
